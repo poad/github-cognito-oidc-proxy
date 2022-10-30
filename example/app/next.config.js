49b98ec8068b4c577bbf3da342a0262671f6e737
Kenji Saito <ken-yo@mbr.nifty.com>
2022-10-31 00:28:24 +0900

    Bumps node modules

--- example/app/next.config.js
+++ example/app/next.config.js
@@ -1,13 +1,11 @@
-/** @type {import('next').NextConfig} */
-const withPreact = require('next-plugin-preact');
 const withBundleAnalyzer = require('@next/bundle-analyzer');
 
+/** @type {import('next').NextConfig} */
 const config = withBundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
-})(withPreact(
-  {
-    reactStrictMode: true,
-    swcMinify: true,
-  }));
+})({
+  reactStrictMode: true,
+  swcMinify: true,
+});
 
 module.exports = config;
