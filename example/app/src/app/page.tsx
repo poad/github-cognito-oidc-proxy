c582a1785d9c993e891a75e5b49d3fffa4689bfe
Kenji Saito <ken-yo@mbr.nifty.com>
2023-05-07 00:54:40 +0900

    Migrate to Next.js 13.4

--- example/app/src/app/page.tsx
+++ example/app/src/app/page.tsx
@@ -1,10 +1,11 @@
+'use client';
 import Head from 'next/head';
 import SignInButton from '../components/SignInButton';
 import useAuth from '../hooks/useAuth';
-import styles from '../styles/Home.module.css';
 import useUserInfo from '../hooks/useUserInfo';
 import Image from 'next/image';
-import { Inter } from '@next/font/google';
+import { Inter } from 'next/font/google';
+import styles from './styles/Home.module.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
