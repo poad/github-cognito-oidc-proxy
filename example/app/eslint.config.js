e90b316ab87ed638b23d7c8560ae5ad18472a8c0
Kenji Saito <ken-yo@mbr.nifty.com>
2025-10-06 12:02:09 +0900

    エラー修正

--- example/app/eslint.config.js
+++ example/app/eslint.config.js
@@ -6,7 +6,7 @@ import stylistic from '@stylistic/eslint-plugin';
 import react from 'eslint-plugin-react';
 import globals from 'globals';
 
-import nextPlugin from '@next/eslint-plugin-next';
+// import nextPlugin from '@next/eslint-plugin-next';
 import reactHooksPlugin from 'eslint-plugin-react-hooks';
 import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
 import flowtypePlugin from 'eslint-plugin-flowtype';
@@ -40,9 +40,9 @@ export default defineConfig(
   eslint.configs.recommended,
   ...tseslint.configs.strict,
   ...tseslint.configs.stylistic,
-  ...compat.config({
-    extends: ['next/core-web-vitals'],
-  }),
+  // ...compat.config({
+  //   extends: ['next/core-web-vitals'],
+  // }),
   // @ts-expect-error ignore type error
   pluginPromise.configs['flat/recommended'],
   {
@@ -79,32 +79,16 @@ export default defineConfig(
       '@stylistic': stylistic,
       react,
       'jsx-a11y': jsxA11yPlugin,
-      // @ts-expect-error ignore type error
-      '@next/next': nextPlugin,
+      // // @ts-expect-error ignore type error
+      // '@next/next': nextPlugin,
       'flow-type': flowtypePlugin,
       'react-hooks': reactHooksPlugin,
     },
     extends: [
       ...compat.config(jsxA11yPlugin.configs.recommended),
+      'react-hooks/recommended',
     ],
-    // @ts-expect-error ignore type error
     rules: {
-      'react/jsx-uses-react': 'off',
-      'react/jsx-uses-vars': 'off',
-      'react/require-render-return': 'off',
-      'react/display-name': 'off',
-      'react/no-direct-mutation-state': 'off',
-      'react/no-string-refs': 'off',
-      'react/jsx-no-undef': 'off',
-      ...nextPlugin.configs.recommended.rules,
-      ...nextPlugin.configs['core-web-vitals'].rules,
-      ...reactHooksPlugin.configs.recommended.rules,
-      '@next/next/no-duplicate-head': 'off',
-      '@next/next/no-img-element': 'error',
-      '@next/next/no-page-custom-font': 'off',
-      // 'import/namespace': 'off',
-      // 'import/no-named-as-default': 'off',
-      // 'import/no-named-as-default-member': 'off',
       '@stylistic/indent': ['error', 2],
       '@stylistic/semi': ['error', 'always'],
       '@stylistic/comma-dangle': ['error', 'always-multiline'],
